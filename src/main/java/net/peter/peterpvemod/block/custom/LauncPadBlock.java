28034a1ed672b59adfa875809b38228128a29913
peter5600 <peter.colquhoun3@gmail.com>
2023-09-23 00:01:00 +0100

    Add Launch Pad

--- src/main/java/net/peter/peterpvemod/block/custom/LauncPadBlock.java
+++ src/main/java/net/peter/peterpvemod/block/custom/LauncPadBlock.java
@@ -2,43 +2,82 @@ package net.peter.peterpvemod.block.custom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MoverType;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 
-public class LauncPadBlock extends Block {
+import javax.annotation.Nullable;
+
+public class LauncPadBlock extends Block implements SimpleWaterloggedBlock {
+    public static final IntegerProperty DIRECTION = IntegerProperty.create("direction", 0, 7);
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
     public LauncPadBlock(Properties properties) {
         super(properties);
+        registerDefaultState(stateDefinition.any().setValue(DIRECTION, 0).setValue(WATERLOGGED, Boolean.FALSE));
     }
 
-    //Called twice on client and twice on server once for each hand
+    @Nullable
     @Override
-    public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand hand, BlockHitResult blockHitResult) {
-
-        if(!level.isClientSide() && hand == InteractionHand.MAIN_HAND){
-            player.sendSystemMessage(Component.literal("Jumpy Booooooi."));
-        }
-
-        return super.use(blockState, level, blockPos, player, hand, blockHitResult);
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState().setValue(DIRECTION, Mth.floor(((180 + context.getRotation()) * 8 / 360) + 0.5F) & 7);
     }
 
     @Override
-    public void stepOn(Level level, BlockPos blockPos, BlockState blockState, Entity entity) {
+    public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
+        player.sendSystemMessage(Component.literal("Hello"));
+        return super.use(blockState, level, blockPos, player, interactionHand, blockHitResult);
 
-        if(entity instanceof LivingEntity livingEntity){
-            livingEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200));
-            livingEntity.addEffect(new MobEffectInstance(MobEffects.LEVITATION, 200));
-            livingEntity.addEffect(new MobEffectInstance(MobEffects.JUMP, 200));
+
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(DIRECTION, WATERLOGGED);
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(DIRECTION, rotation.rotate(state.getValue(DIRECTION), 8));
+    }
+
+    @Override
+    public void stepOn(Level level, BlockPos blockPos, BlockState state, Entity entity){
+        double moveX = 0;
+        double moveZ = 0;
+        double speed = 0.25F;
+        if (!entity.isCrouching()) {
+            //Copied code from minecraft jump logic on liveable entity
+            float f = entity.getYRot() * ((float)Math.PI / 180F);
+            entity.moveRelative(1f, new Vec3(0,20,0));//I added this just to get them some air time
+            entity.setDeltaMovement(entity.getDeltaMovement().add((double)(-Mth.sin(f) * 5F), 2.5D, (double)(Mth.cos(f) * 5F)));
         }
-
-        super.stepOn(level, blockPos, blockState, entity);
+        entity.fallDistance = 0;
     }
 }
