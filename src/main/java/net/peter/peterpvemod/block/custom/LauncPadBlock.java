8c42c2919c4e0414b04265fe7bb0d5da280f69ff
peter5600 <peter.colquhoun3@gmail.com>
2023-09-24 20:24:03 +0100

    Remove old jump block code

--- src/main/java/net/peter/peterpvemod/block/custom/LauncPadBlock.java
+++ src/main/java/net/peter/peterpvemod/block/custom/LauncPadBlock.java
@@ -5,66 +5,24 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.MoverType;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Rotation;
-import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
-import net.minecraft.world.level.material.FluidState;
-import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.Vec3;
 
 import javax.annotation.Nullable;
 
-public class LauncPadBlock extends Block implements SimpleWaterloggedBlock {
-    public static final IntegerProperty DIRECTION = IntegerProperty.create("direction", 0, 7);
-    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-
+public class LauncPadBlock extends Block {
     public LauncPadBlock(Properties properties) {
         super(properties);
-        registerDefaultState(stateDefinition.any().setValue(DIRECTION, 0).setValue(WATERLOGGED, Boolean.FALSE));
-    }
-
-    @Nullable
-    @Override
-    public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return defaultBlockState().setValue(DIRECTION, Mth.floor(((180 + context.getRotation()) * 8 / 360) + 0.5F) & 7);
     }
 
     @Override
     public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
         player.sendSystemMessage(Component.literal("Hello"));
         return super.use(blockState, level, blockPos, player, interactionHand, blockHitResult);
-
-
-    }
-
-    @Override
-    public FluidState getFluidState(BlockState state) {
-        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(DIRECTION, WATERLOGGED);
-    }
-
-    @Override
-    public BlockState rotate(BlockState state, Rotation rotation) {
-        return state.setValue(DIRECTION, rotation.rotate(state.getValue(DIRECTION), 8));
     }
 
     @Override
@@ -72,9 +30,11 @@ public class LauncPadBlock extends Block implements SimpleWaterloggedBlock {
         if (!entity.isCrouching()) {
             //Copied code from minecraft jump logic on liveable entity
             float f = entity.getYRot() * ((float)Math.PI / 180F);
-            entity.moveRelative(1f, new Vec3(0,20,0));//I added this just to get them some air time
             entity.setDeltaMovement(entity.getDeltaMovement().add((double)(-Mth.sin(f) * 5F), 2.5D, (double)(Mth.cos(f) * 5F)));
+
         }
         entity.fallDistance = 0;
+        super.stepOn(level, blockPos, state, entity);
     }
+
 }
